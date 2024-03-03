26113341db30b2f902348a3b41455bd06ca8a8da
Shriya Molakala <shriyamolakala@Shriyas-MBP-2.attlocal.net>
2024-03-02 23:12:10 -0600

    Fixed Liked and Individual Messages Page, and added the iconbar

--- screens/LikedMatches.js
+++ screens/LikedMatches.js
@@ -1,187 +1,393 @@
 import * as React from "react";
-import { Text, StyleSheet, View, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 import { Image } from "expo-image";
 import { useNavigation } from "@react-navigation/native";
-import { Color, FontFamily, FontSize, Border } from "../GlobalStyles";
+import { FontFamily, FontSize, Border, Color, Padding } from "../GlobalStyles";
 
-const LikedMatches = () => {
+const Matches = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.likedMatches}>
-      <Text style={styles.likedMatches1}>Liked Matches</Text>
-      <View style={[styles.rectangleParent, styles.groupChildLayout]}>
-        <Pressable
-          style={[styles.groupChild, styles.groupChildLayout]}
-          onPress={() => navigation.navigate("IndividualMessages")}
-        />
-        <Text style={[styles.thankYouThat, styles.janeDoeClr]}>
-          Thank you! That was very helpful!
-        </Text>
-        <Text style={[styles.janeDoe, styles.janeDoeClr]}>Jane Doe</Text>
-        <Image
-          style={[styles.avatarIcon, styles.avatarIconLayout]}
+    <View style={styles.matches}>
+      <View style={styles.matchesChild} />
+      <Text style={[styles.universityOfTexas, styles.messageThemLaterTypo]}>
+        20, University of Texas at Dallas
+      </Text>
+      <Text style={[styles.janeDoe, styles.textTypo1]}>Jane Doe</Text>
+      
+      <Image
+          style={[styles.avatarIcon1, styles.avatarIconLayout]}
           contentFit="cover"
           source={require("../assets/avatar1.png")}
-        />
-      </View>
-      <Image
-        style={[styles.likedMatchesChild, styles.avatarIconLayout]}
-        contentFit="cover"
-        source={require("../assets/group-101.png")}
       />
-      <View style={styles.button} />
+
+
+
+
       <Pressable
-        style={styles.wrapper}
+        onPress={() => navigation.navigate("IndividualMessages")}
+      >
+       
+     
+      <View style={styles.button}>
+        
+      <Text style={[styles.message, styles.textTypo]}>Message</Text>
+      </View>
+      </Pressable>
+
+
+      <Text style={[styles.yourMatch, styles.textTypo1]}>Liked Matches</Text>
+      <View style={[styles.buttonWrapper, styles.buttonPosition1]}>
+        <View style={[styles.button1, styles.buttonPosition]} />
+      </View>
+      <View style={[styles.buttonContainer, styles.buttonPosition1]}>
+        <View style={[styles.button2, styles.buttonPosition]} />
+      </View>
+      
+      
+
+
+
+      <View style={styles.matchesItem} />
+      <Pressable
+        style={[styles.vector, styles.groupPosition]}
+        onPress={() => navigation.navigate("DisikedMatches")}
+      >
+        <Image
+          style={[styles.icon, styles.iconLayout]}
+          contentFit="cover"
+          source={require("../assets/vector3.png")}
+        />
+      </Pressable>
+
+      <Pressable
+        style={[styles.vectorPreferences, styles.groupPosition]}
+        onPress={() => navigation.navigate("Preferences")}
+      >
+        <Image
+          style={[styles.preferences, styles.preferencesLayout]}
+          contentFit="cover"
+          source={require("../assets/icons8-preferences-32.png")}
+        />
+      </Pressable>
+      
+      <Pressable
+        style={[styles.likeIcon, styles.likeIconPosition]}
         onPress={() => navigation.navigate("LikedMatches")}
       >
+        <Image
+          style={[styles.icon, styles.iconLayout]}
+          contentFit="cover"
+          source={require("../assets/like.png")}
+        />
+      </Pressable>
 
 
       <Pressable
-        style={styles.vector}
-        onPress={() => navigation.navigate("DislikedMatches")}
+        style={[styles.group, styles.groupPosition]}
+        onPress={() => navigation.navigate("Profile")}
       >
-      <Image
-        style={styles.icon}
-        contentFit="cover"
-        source={require("../assets/like.png")}
-      />
-      </Pressable>
-
-
+        <Image
+          style={[styles.icon, styles.iconLayout]}
+          contentFit="cover"
+          source={require("../assets/group.png")}
+        />
       </Pressable>
       <Image
-        style={styles.iconCog}
+        style={[styles.vectorIcon, styles.vectorPosition]}
         contentFit="cover"
-        source={require("../assets/-icon-cog.png")}
+        source={require("../assets/vector4.png")}
       />
       <Image
-        style={styles.vectorIcon}
+        style={[styles.vectorIcon1, styles.iconLayout]}
         contentFit="cover"
-        source={require("../assets/vector3.png")}
+        source={require("../assets/vector5.png")}
       />
+      <Pressable
+        style={[styles.vector1, styles.vectorPosition]}
+        onPress={() => navigation.navigate("LikedMatches")}
+      >
+        <Image
+          style={[styles.icon, styles.iconLayout]}
+          contentFit="cover"
+          source={require("../assets/vector5.png")}
+        />
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  groupChildLayout: {
-    height: 69,
-    width: 297,
-    position: "absolute",
-  },
-  janeDoeClr: {
-    color: Color.colorBlack,
-    position: "absolute",
-  },
   avatarIconLayout: {
     height: 45,
     position: "absolute",
   },
-  likedMatches1: {
-    top: 24,
-    left: 70,
-    fontSize: 35,
-    letterSpacing: 0,
-    fontWeight: "500",
-    color: Color.colorBrown,
-    width: 231,
+  avatarIcon1: {
+    top: 95,
+    left: 15,
+    width: 47,
+  },
+
+ 
+  textTypo: {
+    fontWeight: "600",
+    color: Color.white,
+    fontSize: FontSize.size_mini,
+  },
+  textTypo1: {
     textAlign: "center",
     position: "absolute",
   },
-  groupChild: {
-    top: 0,
-    borderRadius: Border.br_mini,
-    backgroundColor: Color.colorLinen,
-    left: 0,
+  buttonPosition1: {
+    bottom: "29.06%",
+    top: "64.17%",
+    width: "12.5%",
+    height: "6.77%",
+    position: "absolute",
   },
-  thankYouThat: {
-    top: 40,
-    left: 70,
-    fontSize: FontSize.size_7xs,
-    lineHeight: 18,
-    textAlign: "left",
-    width: 121,
-    height: 18,
+
+  preferences: {
+    height: "100%",
+    width: "100%",
+    maxHeight: "100%",
+    maxWidth: "100%",
+
   },
-  janeDoe: {
-    top: 15,
-    left: 70,
-    fontSize: 15,
-    letterSpacing: 1,
-    fontWeight: "700",
-    width: 76,
-    textAlign: "center",
-  },
-  avatarIcon: {
-    top: 15,
-    left: 7,
-    width: 47,
-  },
-  rectangleParent: {
-    top: 88,
-    left: 14,
-  },
-  likedMatchesChild: {
-    top: 516,
-    width: 320,
-    left: 0,
-  },
-  button: {
-    height: "5.88%",
-    width: "10.31%",
-    top: "18.89%",
-    right: "6.88%",
-    bottom: "75.22%",
-    left: "82.81%",
-    borderRadius: Border.br_980xl,
-    backgroundColor: Color.colorLimegreen,
-    shadowColor: "rgba(136, 144, 194, 0.25)",
+
+vectorPreferences: {
+    left: "24%",
+    right: "20%",
+    bottom: "2.58%",
+    width: "7%",
+    height: "3%",
+},
+  buttonPosition: {
+    left: "0%",
+    bottom: "0%",
+    right: "0%",
+    top: "0%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOpacity: 1,
+    elevation: 8,
+    shadowRadius: 8,
     shadowOffset: {
       width: 0,
       height: 3,
     },
-    shadowRadius: 8,
-    elevation: 8,
-    shadowOpacity: 1,
+
+    vector: {
+      left: "24.47%",
+      right: "70.06%",
+      bottom: "2.58%",
+      width: "5.47%",
+      height: "2.67%",
+    },
+    shadowColor: "rgba(136, 144, 194, 0.25)",
+    borderRadius: Border.br_980xl,
+    position: "absolute",
+    overflow: "hidden",
+    width: "100%",
+  },
+  dislikeIconPosition: {
+    height: 20,
+    top: 170,
+    left: 40,
+    position: "absolute",
+  },
+  
+  likeIconPosition: {
+    height: 20,
+    top: 170,
+    left: 310,
+    position: "absolute",
+  },
+  matchesChild: {
+    top: 102,
+    left: 39,
+    borderRadius: Border.br_mini,
+    backgroundColor: Color.colorLinen,
+    width: 242,
+    height: 312,
+    position: "absolute",
+  },
+  universityOfTexas: {
+    top: 125,
+    left: 75,
+    fontSize: 13,
+    color: Color.colorBlack,
+    fontWeight: "500",
+  },
+  text: {
+    top: 105,
+    left: 121,
+    width: 74,
+    height: 16,
+    textAlign: "center",
+    position: "absolute",
+    color: Color.colorBlack,
+    letterSpacing: 1,
+  },
+  janeDoe: {
+    top: 95,
+    left: 70,
+    fontSize: 20,
+    fontWeight: "700",
+    width: 107,
+    height: 23,
+    color: Color.colorBlack,
+    letterSpacing: 1,
+    textAlign: "center",
+  },
+
+  vectorIcon1: {
+    height: "3.33%",
+    width: "5.63%",
+    top: "94.49%",
+    right: "5.47%",
+    bottom: "2.17%",
+    left: "88.91%",
+    position: "absolute",
+  },
+  
+  avatarIcon: {
+    top: 95,
+    left: 1,
+    width: 100,
+    height: 95,
+    position: "absolute",
+  },
+  message: {
+    lineHeight: 12,
+    height: 10,
+    textAlign: "center",
+  },
+  button: {
+    height: "7.27%",
+    width: "50.13%",
+    top: "23%",
+    right: "25.5%",
+    bottom: "16.61%",
+    left: "24.38%",
+    backgroundColor: Color.colorBrown,
+    flexDirection: "row",
+    paddingHorizontal: 0,
+    paddingVertical: Padding.p_4xl,
     justifyContent: "center",
     alignItems: "center",
+    shadowOpacity: 1,
+    elevation: 8,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowColor: "rgba(136, 144, 194, 0.25)",
+    borderRadius: Border.br_980xl,
     position: "absolute",
     overflow: "hidden",
   },
+
+
+  yourMatch: {
+    top: 35,
+    left: 70,
+    fontSize: 35,
+    letterSpacing: 0,
+    color: Color.colorBrown,
+    width: 231,
+    fontWeight: "500",
+  },
+  button1: {
+    backgroundColor: Color.colorLightcoral,
+  },
+  buttonWrapper: {
+    right: "68.44%",
+    left: "19.06%",
+  },
+  button2: {
+    backgroundColor: Color.colorLimegreen,
+  },
+
   icon: {
     height: "100%",
     width: "100%",
-    left: 35,
+    maxHeight: "100%",
+    maxWidth: "100%",
   },
-  wrapper: {
-    left: 273,
-    top: 115,
-    width: 16,
-    height: 16,
+  iconLayout: {
+    maxHeight: "100%",
+    maxWidth: "100%",
+    overflow: "hidden",
+  },
+
+  buttonContainer: {
+    right: "19.69%",
+    left: "67.81%",
+  },
+  dislikeIcon: {
+    left: 71,
+    width: 21,
+  },
+  likeIcon: {
+    left: 71,
+    width: 21,
+  },
+  matchesItem: {
+    left: 228,
+    width: 19,
+  },
+  matchesInner: {
+    top: 620,
+    width: 390,
+    height: 50,
     position: "absolute",
   },
+  vectorPosition: {
+    top: "94.3%",
+    position: "absolute",
+  },
+  groupPosition: {
+    top: "94.74%",
+    position: "absolute",
+  },
+
+  group: {
+    left: "4.22%",
+    right: "91.09%",
+    bottom: "2.44%",
+    width: "7%",
+    height: "4%",
+  },
+  vectorIcon: {
+    height: "3.98%",
+    width: "7.81%",
+    right: "46.25%",
+    bottom: "1.73%",
+    left: "45.94%",
+    maxHeight: "100%",
+    maxWidth: "100%",
+    overflow: "hidden",
+  },
   iconCog: {
-    top: 528,
-    left: 283,
-    width: 25,
+    top: 632,
+    left: 330,
+    width: 30,
     height: 25,
     position: "absolute",
   },
-  vectorIcon: {
-    height: "3.57%",
-    width: "6.25%",
+  vector: {
+    left: "69.38%",
     top: "94.3%",
     right: "24.38%",
     bottom: "2.14%",
-    left: "69.38%",
-    maxWidth: "100%",
-    maxHeight: "100%",
+    width: "6.25%",
+    height: "3.57%",
     position: "absolute",
-    overflow: "hidden",
   },
-  likedMatches: {
-    backgroundColor: Color.colorAntiquewhite,
+  matches: {
+    backgroundColor: '#F0DFCE',
     flex: 1,
     height: 561,
     overflow: "hidden",
@@ -189,4 +395,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LikedMatches;
+export default Matches;
