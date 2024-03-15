feb2124d50ccb40fe4631b41521c242fa683afec
Shriya Molakala <shriyamolakala@Shriyas-MBP-2.attlocal.net>
2024-03-15 17:59:53 -0500

    fixed the dropdown box labels for Preferences Page

--- screens/Preferences.js
+++ screens/Preferences.js
@@ -1,124 +1,213 @@
-//import * as React from "react";
 import React, {useState} from 'react';
+
 import { Text, Switch, StyleSheet, View, Pressable } from "react-native";
+
 import { useNavigation } from "@react-navigation/native";
+
 import { SelectList } from 'react-native-dropdown-select-list'
+
+
+
 
 
 import { Padding, Color, Border, FontSize, FontFamily } from "../GlobalStyles";
 
+
+
 const Preferences = () => {
+
   const navigation = useNavigation();
 
-  const [selected, setSelected] = React.useState("");
+
+
+  const [label, setSelected] = React.useState("");
+
+  
+
   const [isEnabled1, setIsEnabled1] = useState(false);
+
   const [isEnabled2, setIsEnabled2] = useState(false);
+
   const [isEnabled3, setIsEnabled3] = useState(false);
 
+
+
   const toggleSwitch1 = () => setIsEnabled1(previousState => !previousState);
+
   const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+
   const toggleSwitch3 = () => setIsEnabled3(previousState => !previousState);
 
-  const roomTypeData = [
-    {key:'1', value:'Single'},
-    {key:'2', value:'Double'},
-    {key:'3', value:'Studio'},
-    {key:'4', value:'No Preference'},
+
+
+  const random = [
+
+    {key:'1', value:'hi'},
+
   ]
+
+
+
+  
+
+  const roomTypeData = [
+
+    {key:'1', value:'Single'},
+
+    {key:'2', value:'Double'},
+
+    {key:'3', value:'Studio'},
+
+    {key:'4', value:'No Preference'},
+
+  ]
+
+
 
   const leaseDurationData = [
+
     {key:'1', value:'Month-to-month'},
+
     {key:'2', value:'6 months'},
+
     {key:'3', value:'12 months'},
+
     {key:'4', value:'No Preference'},
+
   ]
+
+
 
   const housingTypeData = [
+
     {key:'1', value:'Dorm'},
+
     {key:'2', value:'Apartment'},
+
     {key:'3', value:'House'},
+
     {key:'4', value:'No Preference'},
+
   ]
+
+
 
   const localityData = [
+
     {key:'1', value:'On-campus'},
+
     {key:'2', value:'Near-campus'},
+
     {key:'3', value:'Off-campus'},
+
     {key:'4', value:'No Preference'},
+
   ]
 
+
+
+  
+
   return (
+
     
+
     <View style={styles.preferences}>
+
       <Text style={styles.preferences1}>Preferences</Text>
+
       <View style={styles.button} />
+
       <Pressable
+
         style={styles.findMyRoommateContainer}
+
         onPress={() => navigation.navigate("Matches")}
+
       >
+
       <Text style={styles.findMyRoommate}>Find my Roommate</Text>
+
       </Pressable>
 
+
+
+      
+
+
+
       <View style={styles.selectListContainer}>
-        <SelectList
+
+
+
+        <SelectList 
+
             setSelected={(val) => setSelected(val)} 
             data={roomTypeData} 
             save="value"
+            placeholder = "Room Type"
             
         />
-      
+
+    
         <SelectList
             setSelected={(val) => setSelected(val)} 
+            placeholder="Lease Duration"
             data={leaseDurationData}
             save="value"
-            label="Lease Duration"
         />
+
         
         <SelectList
+            placeholder="Housing Type"
             setSelected={(val) => setSelected(val)} 
             data={housingTypeData}
             save="value"
-            label="Housing Type"
         />
 
         <SelectList
             setSelected={(val) => setSelected(val)} 
             data={localityData}
             save="value"
-            label="Locality"
+            placeholder="Locality"
         />
+
       </View>
 
       <View style={styles.smokerContainer}>
+
         <View style={styles.smokerSwitchContainer}>
+
           <Text style={styles.smokerText}>Smoking</Text>
+
             <Switch
+
               trackColor={{false: '#808080', true: '#00ff00'}}
+
               thumbColor={isEnabled1 ? '#ffffff' : '#f4f3f4'}
+
               ios_backgroundColor="#3e3e3e"
+
               onValueChange={toggleSwitch1}
+
               value={isEnabled1}
+
             />
+
           </View>
+
       </View>
+
+
 
       <View style={styles.drinkerContainer}>
-        <View style={styles.drinkerSwitchContainer}>
-          <Text style={styles.drinkerText}>Drinking</Text>
-            <Switch
-              trackColor={{false: '#808080', true: '#00ff00'}}
-              thumbColor={isEnabled2 ? '#ffffff' : '#f4f3f4'}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitch2}
-              value={isEnabled2}
-            />
-          </View>
-      </View>
 
-      <View style={styles.petsContainer}>
-        <View style={styles.petsSwitchContainer}>
-          <Text style={styles.petsText}>Pets</Text>
+        <View style={styles.drinkerSwitchContainer}>
+
+          <Text style={styles.drinkerText}>Drinking</Text>
+
             <Switch
+
               trackColor={{false: '#808080', true: '#00ff00'}}
               thumbColor={isEnabled3 ? '#ffffff' : '#f4f3f4'}
               ios_backgroundColor="#3e3e3e"
@@ -128,6 +217,22 @@ const Preferences = () => {
           </View>
       </View>
       
+      <View style={styles.petsContainer}>
+
+        <View style={styles.petsSwitchContainer}>
+
+        <Text style={styles.petsText}>Pets</Text>
+
+        <Switch
+
+        trackColor={{false: '#808080', true: '#00ff00'}}
+        thumbColor={isEnabled3 ? '#ffffff' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch3}
+        value={isEnabled3}
+      />
+      </View>
+    </View>
 
     </View>
   );
@@ -150,6 +255,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    top: -25,
   },
   drinkerSwitchContainer: {
     flexDirection: 'row',
@@ -162,6 +268,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    top: -50,
   },
   petsSwitchContainer: {
     flexDirection: 'row',
@@ -171,10 +278,16 @@ const styles = StyleSheet.create({
     marginRight: 265,
   },
   selectListContainer: {
-    marginTop: 75,
+    marginTop: 100,
+    zIndex: 3,
   },
+
+  selectList : {
+    padding: 30,
+  },
+  
   preferences1: {
-    top: 24,
+    top: 35,
     left: 75,
     fontSize: 35,
     letterSpacing: 0,
